008712e10da89074c0b563a6dbcf47ebc195c9a6
迟慧璇 <chihuixuan@chihuixuandeMacBook-Pro.local>
2021-01-12 19:44:57 +0800

    modefy bug,add plot

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ from data import PubmedData
 from sampling import multihop_sampling
 
 from collections import namedtuple
+import matplotlib.pyplot as plt
 
-dataset = "citeseer"
+dataset = "cora"
 assert dataset in ["cora", "citeseer", "pubmed"]
 num_layers = 2
 assert num_layers in [2, 3]
@@ -90,8 +91,11 @@ elif dataset == "citeseer":
 else:
     data = PubmedData().data
 
-x = data.x
-# x = data.x / data.x.sum(1, keepdims=True)  # 归一化数据，使得每一行和为1
+# x = data.x
+if dataset == "citeseer":
+    x = data.x
+else:
+    x = data.x / data.x.sum(1, keepdims=True)  # 归一化数据，使得每一行和为1
 
 # 定义训练、验证、测试集
 train_index = np.where(data.train_mask)[0]
@@ -112,8 +116,17 @@ optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=5e-4)
 
 # 定义训练函数
 def train():
+    train_losses = []
+    train_acces = []
+    val_losses = []
+    val_acces = []
+
     model.train()  # 训练模式
     for e in range(EPOCHS):
+        train_loss = 0
+        train_acc = 0
+        val_loss = 0
+        val_acc = 0
 
         if e % 5 == 0:
             optimizer.param_groups[0]['lr'] *= 0.1
@@ -132,6 +145,7 @@ def train():
             batch_train_logits = model(batch_sampling_x)
             # 计算损失
             loss = criterion(batch_train_logits, batch_src_label)
+            train_loss += loss.item()
 
             # 更新参数
             optimizer.zero_grad()
@@ -142,13 +156,25 @@ def train():
             _, pred = torch.max(batch_train_logits, dim=1)
             correct = (pred == batch_src_label).sum().item()
             acc = correct / BATCH_SIZE
+            train_acc += acc
 
-            val_loss, val_acc = validate()
+            validate_loss, validate_acc = validate()
+            val_loss += validate_loss
+            val_acc += validate_acc
+
             print(
                 "Epoch {:03d} Batch {:03d} train_loss: {:.4f} train_acc: {:.4f} val_loss: {:.4f} val_acc: {:.4f}".format
-                (e, batch, loss.item(), acc, val_loss, val_acc))
+                (e, batch, loss.item(), acc, validate_loss, validate_acc))
+
+        train_losses.append(train_loss / NUM_BATCH_PER_EPOCH)
+        train_acces.append(train_acc / NUM_BATCH_PER_EPOCH)
+        val_losses.append(val_loss / NUM_BATCH_PER_EPOCH)
+        val_acces.append(val_acc / NUM_BATCH_PER_EPOCH)
+
         # 测试
         test()
+
+    res_plot(EPOCHS, train_losses, train_acces, val_losses, val_acces)
 
 
 # 定义测试函数
@@ -177,6 +203,27 @@ def test():
         predict_y = test_logits.max(1)[1]
         accuarcy = torch.eq(predict_y, test_label).float().mean().item()
         print("Test Accuracy: ", accuarcy)
+
+
+def res_plot(epoch, train_losses, train_acces, val_losses, val_acces):
+    epoches = np.arange(0, epoch, 1)
+    plt.figure()
+    ax = plt.subplot(1, 2, 1)
+    # 画出训练结果
+    plt.plot(epoches, train_losses, 'b', label='train_loss')
+    plt.plot(epoches, train_acces, 'r', label='train_acc')
+    # plt.setp(ax.get_xticklabels())
+    plt.legend()
+
+    plt.subplot(1, 2, 2, sharey=ax)
+    # 画出训练结果
+    plt.plot(epoches, val_losses, 'k', label='val_loss')
+    plt.plot(epoches, val_acces, 'g', label='val_acc')
+    plt.legend()
+
+    plt.savefig('res_plot.jpg')
+
+    plt.show()
 
 
 # main函数，程序入口
